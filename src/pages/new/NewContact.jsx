5ba82cf2d24c20256a75b8bcf44ff33839b611e7
Nurjahon1001 <nurjahonalisherova@gmail.com>
2022-05-09 06:29:16 +0300

    groups and phone categories made dynamic

--- src/pages/new/NewContact.jsx
+++ src/pages/new/NewContact.jsx
@@ -3,11 +3,26 @@ import userImage from '../../static/images/user.png'
 import '../../static/css/new.css'
 import TopSide from './TopSide'
 import MainContext from '../../context/MainContext';
+import { useState } from 'react';
+import Select from 'react-select';
+import { useEffect } from 'react';
+import base_url from './../../services/Axios';
 
 function NewContact() {
 
   const { groups, handleAdd } = useContext(MainContext)
   const groupsRef = useRef()
+  const [newContactGroups, setNewContactGroups] = useState([])
+  const [contactTypes, setContactTypes] = useState([])
+  const [hide, setHide] = useState(true)
+  const [type, setType] = useState('')
+
+  useEffect(() => {
+    base_url.get('/contact-types')
+      .then(res => setContactTypes(res.data.map((ct, i) => { return { value: ct.title, label: ct.title } })
+      ))
+      .catch(err => console.log(err))
+  }, [])
 
   const handleSend = (e) => {
     e.preventDefault()
@@ -15,28 +30,67 @@ function NewContact() {
 
     let nC = {
       id: Date.now(),
-      name: target[2].value,
-      email: [
-        {
+      name: '',
+      email: [],
+      phone: [],
+      groups: newContactGroups,
+      is_fv: false,
+      is_fam: false,
+      company: '',
+      birthday: '',
+      address: ''
+    }
+
+    for (let a = 0; a < target.length; a++) {
+      let variable = target[a].name;
+      if (variable === "name") {
+        nC.name = target[a].value
+      } else if (variable === "company"){
+        nC.company = target[a].value
+      } else if (variable === "address"){
+        nC.address = target[a].value
+      } else if (variable === "birthday"){
+        nC.company = target[a].value
+      } else if (variable === "email") {
+        nC.email = [{
           id: Date.now() + 10,
           type: "personal",
-          mail: target[3].value
-        }
-      ],
-      phone: [
-        {
+          mail: target[a].value
+        }]
+      } else if (variable === "phone1") {
+        nC.phone = [{
           id: Date.now() + 100,
-          type: "home",
-          number: target[4].value
-        }
-      ],
-      company: target[5].value,
-      birthday: target[6].value,
-      address: target[7].value
+          type: type,
+          number: target[a].value
+        }]
+      } 
     }
 
     handleAdd(nC)
   }
+
+  const handleGroups = (groupId) => {
+    setNewContactGroups(prev => {
+      if (!prev.includes(groupId)) {
+        return [...prev, groupId]
+      } else {
+        return prev.filter(pr => pr != groupId)
+      }
+    })
+  }
+
+  const handlePhoneChange = (e) => {
+
+    let val = e.target.value;
+
+    if (val >= 100) {
+      setHide(false)
+    } else {
+      setHide(true)
+    }
+  }
+
+
 
   return (
     <>
@@ -45,31 +99,36 @@ function NewContact() {
           image={userImage}
           groups={groups}
           gRef={groupsRef}
+          handleGroups={handleGroups}
+          newContactGroups={newContactGroups}
         />
         <div className='w-50 p-3'>
           <div className="mb-3">
             <label htmlFor="name" className='form-label'>Name</label>
-            <input type="text" id="name" className='form-control' />
+            <input type="text" id="name" name='name' className='form-control' />
           </div>
           <div className="mb-3">
             <label htmlFor="email" className='form-label'>Email</label>
-            <input type="email" id="email" className='form-control' />
+            <input type="email" id="email" name='email' className='form-control' />
           </div>
           <div className="mb-3">
             <label htmlFor="phone" className='form-label'>PhoneNumber</label>
-            <input type="text" id="phone" className='form-control' />
+            <div className="input-group">
+              <input onChange={handlePhoneChange} required type="text" name='phone1' id="phone" className='form-control rounded me-2' />
+              <Select onChange={(data) => setType(data.value)} className={hide ? 'w-50 d-none ms-1' : 'w-50'} options={contactTypes} />
+            </div>
           </div>
           <div className="mb-3">
             <label htmlFor="company" className='form-label'>Company</label>
-            <input type="text" id="company" className='form-control' />
+            <input type="text" id="company" name='company' className='form-control' />
           </div>
           <div className="mb-3">
             <label htmlFor="birthday" className='form-label'>Birthday</label>
-            <input type="date" id="birthday" className='form-control' />
+            <input type="date" id="birthday" name='birthday' className='form-control' />
           </div>
           <div className="mb-3">
             <label htmlFor="address" className='form-label'>Address</label>
-            <input type="text" id="address" className='form-control' />
+            <input type="text" id="address" name='address' className='form-control' />
           </div>
         </div>
       </form>
--- src/pages/new/TopSide.jsx
+++ src/pages/new/TopSide.jsx
@@ -3,7 +3,7 @@ import '../../static/css/new.css'
 
 
 
-function TopSide({ image, groups, gRef }) {
+function TopSide({ image, groups, gRef, handleGroups, newContactGroups }) {
 
     const handleShow = () => {
         gRef.current.classList.toggle('show')
@@ -19,16 +19,23 @@ function TopSide({ image, groups, gRef }) {
                 {/* <div className='image-not-found'>A</div> */}
             </div>
             <div className='d-flex align-items-center position-relative'>
-                <i onClick={handleShow} className='bi bi-tag-fill text-secondary ms-5 rotate-icon' />
+                <i onClick={handleShow} className='bi bi-tag-fill text-secondary ms-4 rotate-icon' />
                 <div ref={gRef} className="group-list">
                     <ul className="list-group">
-                        {groups.map(group => (<li
-                            key={group.id}
-                            className='list-group-item border-0'
-                        >
-                            <i className='bi bi-tag me-2' />
-                            {group.title.slice(0, 17)} {group.title.length > 17 ? "..." : ""}
-                        </li>))}
+                        {groups.map(group => {
+                            return (
+                                <li
+                                    key={group.id}
+                                    className={newContactGroups.includes(group.id) ? 'list-group-item border-0 hover-li include d-flex justify-content-between' : 'list-group-item border-0 hover-li d-flex justify-content-between'}
+                                    onClick={() => handleGroups(group.id)}
+                                >
+                                    <div>
+                                        <i className='bi bi-tag me-2 rotate' />
+                                        {group.title.slice(0, 17)} {group.title.length > 17 ? "..." : ""}
+                                    </div>
+                                    {newContactGroups.includes(group.id) ? <i className='bi bi-check include'></i> : ""}
+                                </li>)
+                        })}
                     </ul>
                 </div>
             </div>
